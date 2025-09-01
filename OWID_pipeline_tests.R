304c64d9f14d00cdc0fee39309ed4b155618a128
goncalomarques8 <goncalojosemarques@gmail.com>
2025-09-01 17:43:47 +0200

    Keeping the essential

--- OWID_pipeline_tests.R
+++ OWID_pipeline_tests.R
@@ -11,7 +11,7 @@ assertthat::assert_that(
 ) # if TRUE we are sure to be working with the most updated set of countries and datasets as well.
 
 
-# auxilliary function `share_below_half_median()` non-existing in lissyrtools ------
+# Auxilliary function `share_below_half_median()` non-existing in lissyrtools ------------------------------
 
 share_below_half_median <- function(
   data_list,
@@ -73,7 +73,7 @@ share_below_half_median <- function(
 }
 
 
-# loading the data  ---------------------------------------------------------------
+# Loading the data  ---------------------------------------------------------------
 variables_owid <- c("dhi", "hifactor", "hiprivate", "hi33", "grossnet")
 
 tic()
@@ -83,64 +83,10 @@ full_data_hhd <- lissyuse(
 ) # 152.78 sec elapsed / 172.73
 toc()
 
-# prel analysis of how mi (market income) is conditioned according to OWID.  ---------------------------------------------------------------
 
-# 1) the maximum values of its elements
-
-# if `max_hifactor' > 0 & `max_hiprivate' > 0 & `max_hi33' > 0 {
-#		  gen mi = hifactor + hiprivate + hi33
-#	} else {
-#		  gen mi = .
-#}
-
-# 2) interaction with `grossnet`
-
-# if "`var'" == "mi" & ("`uniq_gross'" == "200" | "`uniq_gross'" == "300" | "`uniq_gross'" == "310" | "`uniq_gross'" == "320" | "`uniq_gross'" == ".") {
-#			replace e`var'_b = .
-#	}
-
-check_mi_elements_max_and_grossnet <- map2(
-  full_data_hhd,
-  stringr::str_sub(names(full_data_hhd), 1, 4),
-  ~ .x %>%
-    summarise(
-      dname = .y,
-      max_hifactor = max(hifactor, na.rm = TRUE),
-      max_hiprivate = max(hiprivate, na.rm = TRUE),
-      max_hi33 = max(hi33, na.rm = TRUE),
-      grossnet = mean(grossnet)
-    )
-) %>%
-  list_rbind()
-
-# check dname (rows) where at least one of the columns (maximums) is equal to zero
-check_mi_elements_max_and_grossnet %>%
-  mutate(
-    has_at_least_one_zero = if_else(
-      (max_hifactor == 0 | max_hiprivate == 0 | max_hi33 == 0),
-      1,
-      0
-    )
-  ) %>%
-  filter(
-    (has_at_least_one_zero == 1 | grossnet >= 200)
-  ) %>%
-  View()
-
-# assess which dname have them all == 0 (more out of curiosity)
-check_mi_elements_max_and_grossnet %>%
-  mutate(sum_of_maxs = max_hifactor + max_hiprivate + max_hi33) %>%
-  filter(sum_of_maxs == 0) # "us63" "us64" "us65" "us66" #
-
-
-# also no Taiwan in the data
-
-data_no_tw <- full_data_hhd[
-  stringr::str_sub(names(full_data_hhd), 1, 2) != "tw"
-]
-
+# Market Income  ---------------------------------------------------------------
 data_with_mi <- purrr::map(
-  data_no_tw,
+  full_data_hhd,
   ~ .x %>%
     mutate(
       mi = rowSums(across(c("hifactor", "hiprivate", "hi33")), na.rm = TRUE)
@@ -163,9 +109,23 @@ data_with_mi <- purrr::map(
     )
 )
 
+# Preparation of the data (with and without PPP's) --------------------
 
+# 1. Without PPP adjustment (including Taiwan and ru22 for example)
 prep_data <- purrr::map(
   data_with_mi,
+  ~ .x %>%
+    filter(!is.na(dhi)) %>%
+    mutate(new_wgt = hwgt * nhhmem)
+) %>%
+  apply_iqr_top_bottom_coding("dhi", "hwgt", times = 3, type = "type_2") %>% # type = "type_2" ?
+  apply_iqr_top_bottom_coding("mi", "hwgt", times = 3, type = "type_2") %>% # type = "type_2" ?
+  apply_sqrt_equivalisation("dhi") %>% # purrr::map( ~ .x %>% mutate(dhi = dhi / nhhmem)) %>% 
+  apply_sqrt_equivalisation("mi") # # purrr::map( ~ .x %>% mutate(mi = mi / nhhmem))
+
+# 2. With PPP adjustment (including Taiwan and ru22 for example)
+prep_data_ppp_adj <- purrr::map(
+  data_with_mi[(str_sub(names(data_with_mi),1,2) != "tw") & (str_sub(names(data_with_mi),1,4) != "ru22")],
   ~ .x %>%
     filter(!is.na(dhi)) %>%
     mutate(new_wgt = hwgt * nhhmem)
@@ -178,79 +138,14 @@ prep_data <- purrr::map(
   apply_sqrt_equivalisation("mi") 
 # in terms of the preparation, we are on a very good path of alignment with OWID figures computed in Stata.
 
-# Let us comapre the equivalized dhi values with the ones in Stata (using both `summarize` and `collapse` commands)
 
-output_comparisson <- prep_data %>%
-  run_weighted_mean("dhi", "new_wgt") %>%
-  structure_to_plot() %>%
-  rename(ey_R = value) %>%
-  arrange(dname) %>%
-  left_join(
-    haven::read_dta(
-      "S:\\Staff\\Goncalo\\2025\\pipeline_OWID\\outputs\\means_summarize.dta"
-    ),
-    by = c("cname", "year", "dname")
-  ) %>%
-  rename(ey_summarize = ey) %>%
-  left_join(
-    haven::read_dta(
-      "S:\\Staff\\Goncalo\\2025\\pipeline_OWID\\outputs\\means_collapse.dta"
-    ),
-    by = c("cname", "year", "dname")
-  ) %>%
-  rename(ey_collapse = ey) %>%
-  filter(dname != "ru22") %>%
-  mutate(
-    abs_dif_R_summarize = abs(ey_R - ey_summarize),
-    abs_dif_R_collapse = abs(ey_R - ey_collapse),
-    abs_dif_sumrz_collapse = abs(ey_summarize - ey_collapse)
-  )
 
-# Export
-output_comparisson %>%
-  readr::write_csv(
-    "S:\\Staff\\Goncalo\\2025\\pipeline_OWID\\outputs\\comparisson_output_R_sum_collapse.csv"
-  )
+# Aggregate Figures  --------------------------------------
 
-# error analysis
-output_comparisson %>%
-  summarise(
-    mean_error_R_sum = mean(abs_dif_R_summarize),
-    mean_error_R_collapse = mean(abs_dif_R_collapse),
-    max_error_R_sum = max(abs_dif_R_summarize),
-    max_error_R_collapse = max(abs_dif_R_collapse)
-  ) # lissyrtools::run_weighted_percentiles (type = "type_2") is more aligned with `collapse`. `collapse` aligns better in practise with definition 2 from Hyndman & Fan 1996.
-
-output_comparisson %>%
-  filter(abs_dif_sumrz_collapse != 0) %>%
-  select(-starts_with("abs_dif_R"))
-
-#'# A tibble: 4 × 7
-#'  cname          year dname   ey_R ey_summarize ey_collapse abs_dif_sumrz_collapse
-#'1 Brazil         2015 br15  10439.       10439.      10439.                0.00293
-#'2 Japan          2010 jp10  30113.       30113.      30113.                0.00195
-#'3 Mexico         2005 mx05   8402.        8402.       8402.                0.146
-#'4 United States  1969 us69  30469.       30469.      30469.                0.00195
-
-# If we opt to proceed with type 2, (I honestly wouldn't for consistency with the method that we are going to use when computing the shares and averages by percentile/decile etc...)
-# Future misalignments can still have their origin in the differences betweem the results of `summarize , det` (to get the percentiles in Stata) and the `collapse` command.
-# This might occur in the top-bottom coding stage, during the preparation, or during the computation of the aggreagte figure itself.
-
-# --------------------------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------------------------
-
-# Using prep_data without "ru22h"
-prep_data <- prep_data[!(names(prep_data)) == "ru22h"]
-
-# Inequality
-
-# 1) Gini
-
-gini <- prep_data %>%
-  run_weighted_gini("dhi", "new_wgt") %>%
-  structure_to_plot() %>%
-  mutate(indicator = "Gini Index", variable = "dhi", equiv = "square root")
+gini <- prep_data %>% #list
+  run_weighted_gini("dhi", "new_wgt") %>% # dedicated function, that only asks for a variable and a weight
+  structure_to_plot() %>% # Function to restructure a list into a tidy data frame
+  mutate(indicator = "Gini Index", variable = "dhi", equiv = "square root") # addition of extra columns
 
 share_richest_10 <- prep_data %>%
   run_weighted_percentiles("dhi", "new_wgt", probs = c(0.9), share = TRUE) %>%
@@ -280,7 +175,7 @@ palma_ratio <- prep_data %>%
   select(-`0-40%`, -`90-100%`) %>%
   mutate(indicator = "Palma Ratio", variable = "dhi", equiv = "square root")
 
-share_below_50_median <- prep_data[1:4] %>%
+share_below_50_median <- prep_data %>%
   share_below_half_median("dhi", "new_wgt") %>%
   structure_to_plot() %>%
   mutate(
@@ -320,17 +215,17 @@ ratio_50_10 <- prep_data %>%
   mutate(indicator = "Ratio p50_p10", variable = "dhi", equiv = "square root")
 
 
-average <- prep_data %>%
+average <- prep_data_ppp_adj %>%
   run_weighted_mean("dhi", "new_wgt") %>%
   structure_to_plot() %>%
   mutate(indicator = "Average", variable = "dhi", equiv = "square root")
 
-median <- prep_data %>%
+median <- prep_data_ppp_adj %>%
   run_weighted_percentiles("dhi", "new_wgt", probs = c(0.5)) %>%
   structure_to_plot() %>%
   mutate(indicator = "Median", variable = "dhi", equiv = "square root")
 
-deciles <- prep_data[1:4] %>%
+deciles <- prep_data_ppp_adj %>%
   run_weighted_percentiles("dhi", "new_wgt", probs = seq(0.1, 0.9, 0.1)) %>%
   structure_to_plot() %>%
   mutate(
@@ -340,7 +235,7 @@ deciles <- prep_data[1:4] %>%
   ) %>%
   select(-category)
 
-deciles_shares <- prep_data[1:4] %>%
+deciles_shares <- prep_data %>%
   run_weighted_percentiles(
     "dhi",
     "new_wgt",
@@ -354,7 +249,7 @@ deciles_shares <- prep_data[1:4] %>%
     equiv = "square root"
   )
 
-deciles_mean <- prep_data[1:4] %>%
+deciles_mean <- prep_data_ppp_adj %>%
   run_weighted_percentiles(
     "dhi",
     "new_wgt",
@@ -369,7 +264,7 @@ deciles_mean <- prep_data[1:4] %>%
   )
 
 
-percentiles <- prep_data %>%
+percentiles <- prep_data_ppp_adj %>%
   run_weighted_percentiles("dhi", "new_wgt", probs = seq(0.01, 0.99, 0.01)) %>%
   structure_to_plot() %>% 
   mutate( 
@@ -393,7 +288,7 @@ percentiles_shares <- prep_data %>%
     equiv = "square root"
   )
 
-percentiles_mean <- prep_data %>%
+percentiles_mean <- prep_data_ppp_adj %>%
   run_weighted_percentiles(
     "dhi",
     "new_wgt",
@@ -420,7 +315,7 @@ thresholds <- c(
 results <- list()
 for (line in thresholds) {
   a <- run_weighted_relative_poverty(
-    prep_data[1:3],
+    prep_data,
     var_name = "dhi",
     wgt_name = "new_wgt",
     times_median = line
@@ -499,7 +394,7 @@ poverty_lines <- c(
 results <- list()
 for (line in poverty_lines) {
   a <- run_weighted_absolute_poverty(
-    data_list = prep_data[1:3],
+    data_list = prep_data_ppp_adj,
     var_name = "dhi",
     wgt_name = "new_wgt",
     daily_poverty_line = line
@@ -513,11 +408,11 @@ for (line in poverty_lines) {
   results[[as.character(line)]] <- a
 }
 final_df <- bind_rows(results)
-print(final_df)
+
 
 
 run_weighted_absolute_poverty(
-  prep_data,
+  prep_data_ppp_adj,
   var_name = "dhi",
   wgt_name = "new_wgt",
   daily_poverty_line = line
@@ -530,7 +425,7 @@ run_weighted_absolute_poverty(
   )
 
 run_weighted_poverty_shortfall(
-  prep_data,
+  prep_data_ppp_adj,
   var_name = "dhi",
   wgt_name = "new_wgt",
   daily_poverty_line = line,
@@ -543,7 +438,7 @@ run_weighted_poverty_shortfall(
     equiv = "square root"
   )
 run_weighted_poverty_shortfall(
-  prep_data,
+  prep_data_ppp_adj,
   var_name = "dhi",
   wgt_name = "new_wgt",
   daily_poverty_line = line,
@@ -558,7 +453,7 @@ run_weighted_poverty_shortfall(
 
 
 run_weighted_poverty_gap_index(
-  prep_data,
+  prep_data_ppp_adj,
   var_name = "dhi",
   wgt_name = "new_wgt",
   daily_poverty_line = line
@@ -571,7 +466,3 @@ run_weighted_poverty_gap_index(
   )
 
 
-
-
-all_relative_poverty_metrics
-all_absolute_poverty_metrics
